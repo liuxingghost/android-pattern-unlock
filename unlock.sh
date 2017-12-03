06737656ed1ec985519170466727ee51c8754ee7
Prince Paul <princepaul@users.noreply.github.com>
2017-12-03 11:42:33 +0530

    Update unlock.sh
    Modified the script for Xiaomi Devices and values

--- unlock.sh
+++ unlock.sh
@@ -2,6 +2,9 @@
 #
 # Android pattern unlock
 # Author: Matt Wilson
+# Modified : Prince Paul
+# Last Modified : 03 Dec 2017
+# Note : Modded for Xiaomi Devices
 # Licence: Free to use and share. If this helps you please buy me a beer :)
 #
 # This script sends simulated touch input over ADB for remotely swiping on Android's pattern lockscreen.
@@ -32,23 +35,23 @@
 
 PATTERN="1 2 3 6 5 4 7 8 9" # The unlock pattern to draw, space seperated
 
-COL_1=166                   # X coordinate of column 1 (in pixels)
-COL_2=384                   # X coordinate of column 2 (in pixels)
-COL_3=576                   # X coordinate of column 3 (in pixels)
+COL_1=240                   # X coordinate of column 1 (in pixels)
+COL_2=540                   # X coordinate of column 2 (in pixels)
+COL_3=840                   # X coordinate of column 3 (in pixels)
 
-ROW_1=553                   # Y coordinate of row 1 (in pixels)
-ROW_2=738                   # Y coordinate of row 2 (in pixels)
-ROW_3=984                   # Y coordinate of row 3 (in pixels)
+ROW_1=1025                   # Y coordinate of row 1 (in pixels)
+ROW_2=1325                   # Y coordinate of row 2 (in pixels)
+ROW_3=1625                   # Y coordinate of row 3 (in pixels)
 
-MULTIPLIER=2                # Multiplication factor for coordinates. For Nexus 4, set this to 2. For low res phones such as
+MULTIPLIER=1                # Multiplication factor for coordinates. For Nexus 4, set this to 2. For low res phones such as
                             # Samsung Galaxy S2, set this to 1. Experiment with this value if you can't see anything happening.
 
 WAKE_SCREEN_ENABLED=true    # If true, the script will start by sending the power button press event
 
 SWIPE_UP_ENABLED=true       # If true, the script will swipe upwards before drawing the pattern (e.g. for lollipop lockscreen)
 SWIPE_UP_X=450              # X coordinate for initial upward swipe. Only used if SWIPE_UP_ENABLED is true
-SWIPE_UP_Y_FROM=1000        # Start Y coordinate for initial upward swipe. Only used if SWIPE_UP_ENABLED is true
-SWIPE_UP_Y_TO=200           # End Y coordinate for initial upward swipe. Only used if SWIPE_UP_ENABLED is true
+SWIPE_UP_Y_FROM=1900        # Start Y coordinate for initial upward swipe. Only used if SWIPE_UP_ENABLED is true
+SWIPE_UP_Y_TO=400           # End Y coordinate for initial upward swipe. Only used if SWIPE_UP_ENABLED is true
 
 # =======================================================================================================================
 
@@ -93,10 +96,10 @@ StartTouch() {
 }
 
 SendCoordinates () {
-	adb shell sendevent /dev/input/event2 3 53 $1
-	adb shell sendevent /dev/input/event2 3 54 $2
-	adb shell sendevent /dev/input/event2 3 58 57
-	adb shell sendevent /dev/input/event2 0 0 0
+	adb shell sendevent /dev/input/event1 3 53 $1
+	adb shell sendevent /dev/input/event1 3 54 $2
+	adb shell sendevent /dev/input/event1 1 330 1
+	adb shell sendevent /dev/input/event1 0 0 0
 }
 
 FinishTouch() {
